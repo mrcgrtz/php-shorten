39350bcc01f4303da392158a73171330d3d33a1e
Marc Görtz <mail@marcgoertz.de>
2020-06-26 10:47:55 +0200

    Fix tests.

--- tests/ShortenTest.php
+++ tests/ShortenTest.php
@@ -1,50 +1,62 @@
 <?php
 declare(strict_types=1);
 
-use Marcgoertz\Shorten\Shorten;
 use PHPUnit\Framework\TestCase;
+use Marcgoertz\Shorten\Shorten;
 
 final class ShortenTest extends TestCase
 {
     public function testTruncatesMarkup(): void
     {
+        $shorten = new Shorten();
         $this->assertEquals(
             '<a href="https://example.com/">Go to exam</a>…',
-            Shorten::truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10)
+            $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10)
         );
     }
 
     public function testTruncatesMarkupWithAppendixOutside(): void
     {
+        $shorten = new Shorten();
         $this->assertEquals(
             '<a href="https://example.com/">Go to</a>…',
-            Shorten::truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', FALSE, TRUE)
+            $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', FALSE, TRUE)
         );
     }
 
     public function testTruncatesMarkupWithAppendixInside(): void
     {
+        $shorten = new Shorten();
         $this->assertEquals(
             '<a href="https://example.com/">Go to…</a>',
-            Shorten::truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', TRUE, TRUE)
+            $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', TRUE, TRUE)
         );
     }
 
     public function testTruncatesMarkupOnlyIfNeeded(): void
     {
+        $shorten = new Shorten();
         $this->assertEquals(
             'Lorem ipsum <b>dolor</b> sit amet',
-            Shorten::truncateMarkup('Lorem ipsum <b>dolor</b> sit amet', 26)
+            $shorten->truncateMarkup('Lorem ipsum <b>dolor</b> sit amet', 26)
+        );
+    }
+
+    public function testTruncatesMarkupWithEntities(): void
+    {
+        $shorten = new Shorten();
+        $this->assertEquals(
+            '<p>PHP &eacute;l&eacute;…</p>',
+            $shorten->truncateMarkup('<p>PHP &eacute;l&eacute;phant</p>', 7, '…', TRUE)
         );
     }
 
     public function testTruncatesMarkupWithUnicodeChars(): void
     {
+        $shorten = new Shorten();
         $this->assertEquals(
             '<p>PHP élé…</p>',
-            Shorten::truncateMarkup('<p>PHP éléphant</p>', 7, '…', TRUE)
+            $shorten->truncateMarkup('<p>PHP éléphant</p>', 7, '…', TRUE)
         );
     }
 }
-
-
