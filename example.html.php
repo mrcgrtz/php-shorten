d78cf4e30a93ee60a9b87c426afe88758e0ecf05
Marc Görtz <mail@marcgoertz.de>
2021-12-08 23:39:06 +0100

    Update sample code.

--- example.html.php
+++ example.html.php
@@ -1,32 +1,30 @@
-<!DOCTYPE html>
 <?php
-require_once('Shorten.php');
+require_once 'Shorten.php';
 $shorten = new \Marcgoertz\Shorten\Shorten();
 ?>
+<!doctype html>
+<meta charset=utf-8>
+<title>Shorten examples</title>
 <p>
     <?php
         // provides: <a href="https://example.com/">Go to exam</a>…
         print $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10);
     ?>
-</p>
 
 <p>
     <?php
         // provides: <a href="https://example.com/">Go to</a>…
         print $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', false, true);
     ?>
-</p>
 
 <p>
     <?php
         // provides: <a href="https://example.com/">Go to…</a>
         print $shorten->truncateMarkup('<a href="https://example.com/">Go to example site</a>', 10, '…', true, true);
     ?>
-</p>
 
 <p>
     <?php
         // provides: Lorem ipsum <b>dolor</b> sit amet
         print $shorten->truncateMarkup('Lorem ipsum <b>dolor</b> sit amet', 26);
     ?>
-</p>
